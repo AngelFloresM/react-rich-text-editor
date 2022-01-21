b46cf4afd85a690370f5003ccebf6f47a25a538f
AngelFloresM <angelflores992@gmail.com>
2022-01-21 12:58:02 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, SyntheticEvent, useState, ChangeEvent } from "react";
+import "./App.css";
 
 function App() {
+  const paragraphRef = useRef<HTMLTextAreaElement>(null);
+  const [text, setText] = useState(
+    "This is something that can be selected, entirely or just a portion of it"
+  );
+
+  const handleSelect = (e: SyntheticEvent<HTMLTextAreaElement>) => {
+    const start = paragraphRef.current?.selectionStart as number;
+    const end = paragraphRef.current?.selectionEnd as number;
+    const selectedText = e.currentTarget.value.substring(start, end);
+
+    if (selectedText !== "") {
+      console.log(selectedText);
+      const newText = text.replace(selectedText, "texto remplazado");
+      setText(newText);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        ref={paragraphRef}
+        onSelect={handleSelect}
+        onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+          setText(event.currentTarget.value)
+        }
+        cols={40}
+        rows={5}
+        value={text}
+      />
     </div>
   );
 }
